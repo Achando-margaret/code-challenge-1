0bc3a8abeb4c1059def11eebe35e2126eea6022c
margaret <margaret.achando@student.moringaschool.com>
2024-03-24 20:38:13 +0300

    CreatesCodeChallenge1

---
+++ speeddectator.js
@@ -0,0 +1,16 @@
+function calculateDemeritPoints(speed) {
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+    const demeritPointsThreshold = 12;
+  
+    if (speed <= speedLimit) {
+        console.log("Ok");
+    } else {
+        const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+        if (demeritPoints > demeritPointsThreshold) {
+            console.log("License suspended");
+        } else {
+            console.log("Points:", demeritPoints);
+        }
+    }
+}
